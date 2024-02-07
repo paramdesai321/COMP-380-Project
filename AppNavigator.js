4e86494080c7de05a4ff43d16ae1af03dfb1a5f9
Desai <param-ridham.desai.167@my.csun.edu>
2024-02-07 00:12:22 -0800

    clearning stash

---
+++ AppNavigator.js
@@ -0,0 +1,16 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './components/Login';
+// Import other screens here
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default AppNavigator;
