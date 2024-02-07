3982b736bf909c30d809ffd6cf193da6733cf259
Desai <param-ridham.desai.167@my.csun.edu>
2024-02-07 00:11:34 -0800

    made some changes and fixed some errors in App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import AppNavigator from './AppNavigator'; // Import your AppNavigator component
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <AppNavigator /> 
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
