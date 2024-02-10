642a841c492127d43dc7d2c898c7c9790eb1e65d
Desai <param-ridham.desai.167@my.csun.edu>
2024-02-10 12:33:38 -0800

    styling the header tab bar

--- AppNavigator.js
+++ AppNavigator.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Login from './components/Login';
 import Signup from './components/SignUp';
+import { StyleSheet } from 'react-native';
 // Import other screens here
 
 const Stack = createStackNavigator();
@@ -9,11 +10,45 @@ const Stack = createStackNavigator();
 const AppNavigator = () => (
   <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name= "SignUp" component={Signup}/>
+      <Stack.Screen name="Login" component={Login} 
+      options={{
+        title: 'Login',
+        headerStyle: {
+          backgroundColor: 'yellow',
+          
+        },
+        headerTintColor: 'black',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+      />
+      <Stack.Screen name= "SignUp" component={Signup}
+        options={{
+          title: 'Create an account',
+          headerStyle: {
+            backgroundColor: 'yellow',
+            
+          },
+          headerTintColor: 'black',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      
+      />
     
     </Stack.Navigator>
   </NavigationContainer>
 );
+
+const styles = StyleSheet.create({
+
+  container:{
+    backgroundColor: 'Yellow'
+  }
+
+});
+
 
 export default AppNavigator;
