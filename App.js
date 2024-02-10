a7c31506c13c20ee612bb696a61dacc0e3d33073
Vica Mit <victoria.miteva.929@my.csun.edu>
2024-02-10 13:33:20 -0800

    basic bottom nav bar added

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import BottomNavBar from './components/BottomNavBar';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello world!</Text>
       <StatusBar style="auto" />
+      <BottomNavBar />
     </View>
   );
 }
---
+++ components/BottomNavBar.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+const BottomNavBar = () => {
+  const [selectedTab, setSelectedTab] = useState('myStats');
+
+  const handleTabPress = (tabName) => {
+    setSelectedTab(tabName);
+    // You can add logic here to navigate to different screens based on the selected tab
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={[styles.tab, selectedTab === 'myStats' && styles.selectedTab]}
+        onPress={() => handleTabPress('myStats')}>
+        <Icon name="activity" size={24} color="black" style={[selectedTab === 'myStats' && styles.selectedTabIcon]}/>
+        <Text style={[styles.tabText]}>My Stats</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity
+        style={[styles.tab, selectedTab === 'Inbox' && styles.selectedTab]}
+        onPress={() => handleTabPress('Inbox')}>
+        <Icon name="message-circle" size={24} color="black" style={[selectedTab === 'Inbox' && styles.selectedTabIcon]} />
+        <Text style={styles.tabText}>Inbox</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.tab, selectedTab === 'Explore' && styles.selectedTab]}
+        onPress={() => handleTabPress('Explore')}>
+        <Icon name="search" size={24} color="black" style={[selectedTab === 'Explore' && styles.selectedTabIcon]}/>
+        <Text style={styles.tabText}>Explore</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.tab, selectedTab === 'Friends' && styles.selectedTab]}
+        onPress={() => handleTabPress('Friends')}>
+        <Icon name="users" size={24} color="black" style={[selectedTab === 'Friends' && styles.selectedTabIcon]}/>
+        <Text style={styles.tabText}>Friends</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.tab, selectedTab === 'Profile' && styles.selectedTab]}
+        onPress={() => handleTabPress('Profile')}>
+        <Icon name="user" size={24} color="black" style={[selectedTab === 'Profile' && styles.selectedTabIcon]}/>
+        <Text style={styles.tabText}>Profile</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+    paddingBottom: 5,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    marginBottom: 15
+  },
+  tab: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  selectedTab: {
+    borderBottomWidth: 2,
+    borderBottomColor: 'green',
+  },
+  tabText: {
+    fontSize: 16,
+    marginTop: 10
+  },
+  selectedTabIcon: {
+    color: 'green', // Change the color to highlight the selected tab text
+  },
+});
+
+export default BottomNavBar;
