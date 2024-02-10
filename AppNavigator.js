41e84b2858116b6469b4369d9345113b54a3ba95
Desai <param-ridham.desai.167@my.csun.edu>
2024-02-10 12:12:52 -0800

    adding signup screen

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Login from './components/Login';
+import Signup from './components/SignUp';
 // Import other screens here
 
 const Stack = createStackNavigator();
@@ -9,6 +10,7 @@ const AppNavigator = () => (
   <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name= "SignUp" component={Signup}/>
     
     </Stack.Navigator>
   </NavigationContainer>
