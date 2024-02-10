88f8fdbda375d7b489aa9b1cb7f986230526e20c
Desai <param-ridham.desai.167@my.csun.edu>
2024-02-10 13:39:00 -0800

    styling

--- components/SplashScreen.js
+++ components/SplashScreen.js
@@ -1,74 +1,59 @@
-import {View, TextInput, Button, TouchableOpacity, StyleSheet,Text, ImageBackground,Style} from 'react-native';
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet, Text, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+const SplashScreen = () => {
+  const navigation = useNavigation();
 
-const SplashScreen=()=>{
+  const handleLogin = () => {
+    navigation.navigate('Login');
+  };
 
-   // const buttonOpacity =  password && email ? 1 : 0.5;
-    const navigation= useNavigation();
-   const handleLogin=()=>{
-     navigation.navigate('Login');
-   }
-
-   const handleSignUp=()=>{
+  const handleSignUp = () => {
     navigation.navigate('SignUp');
-  }
+  };
 
-   
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/Harmony.png')}
+        resizeMode="contain"
+        style={{ width: 200, height: 400 }}
+      />
+      <Text style={styles.welcomeText}>Welcome to Harmony</Text>
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleSignUp} style={styles.button}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-return(
-
-<View style={styles.container}>
-<TouchableOpacity
-onPress={handleLogin}
-style={styles.button}
->
-
-
-<Text style={styles.buttonText}>Login</Text>
-</TouchableOpacity>
-<TouchableOpacity
-onPress={handleSignUp}
-style={styles.button}
->
-
-
-<Text style={styles.buttonText}>Sign Up</Text>
-</TouchableOpacity>
-
-</View>
-
-
-);
-}
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 20,
-    },
-    input: {
-      width: '100%',
-      borderWidth: 1,
-      borderColor: '#ccc',
-      padding: 10,
-      marginBottom: 10,
-    },
-    button: {
-      backgroundColor: '#007bff',
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 5,
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: 16,
-      textAlign: 'center',
-    },
-  });
-
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  welcomeText: {
+    fontSize: 24, 
+    marginBottom: 20, 
+  },
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
 
 export default SplashScreen;
-
