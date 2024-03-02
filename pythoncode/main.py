c93bcc0c89385d9b8474aae461a8fcfefd476807
Param Desai <param-ridham.desai.167@my.csun.edu>
2024-03-02 13:51:01 -0800

    changing from local host to IP address of the computer

--- pythoncode/main.py
+++ pythoncode/main.py
@@ -1,21 +1,21 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'helloworld'
-socketio = SocketIO(app,cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 def handle_message(message):
-    print("Received message: "+message)
-    if message != "Used Connected!":
-        (send(message, broadcast=True)
+    print("Received message: " + message)
+    if message != "User Connected!":
+        send(message, broadcast=True)
 
 
-@app.route('./templates/index.html'))
+@app.route('/   ')
 def index():
     return render_template("index.html")
 
-if __name__ == '__main__':
-    socketio.run(app, host="localhost")
 
+if __name__ == '__main__':
+    socketio.run(app, host="192.168.1.250")
