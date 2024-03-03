28471599184acf238fa590e1b2c01a6cb794aee9
Param Desai <param-ridham.desai.167@my.csun.edu>
2024-03-02 16:55:57 -0800

    turning the socket on

--- pythoncode/main.py
+++ pythoncode/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'helloworld'
 socketio = SocketIO(app, cors_allowed_origins="*",logger=True, engineio_logger=True)
 
-
+@socketio.on('message')
 def handle_message(message):
     print("Received message: " + message)
     if message != "User Connected!":
@@ -18,4 +18,4 @@ def index():
 
 
 if __name__ == '__main__':
-    socketio.run(app,host="192.168.1.250",port=5000,allow_unsafe_werkzeug=True)
+    socketio.run(app,host="192.168.1.250",allow_unsafe_werkzeug=True)
