77d6e29547ae691a3b312b55e248f756c3fa2048
Param Desai <param-ridham.desai.167@my.csun.edu>
2024-03-11 15:03:58 -0700

    updating IPV4 address

--- pythoncode/main.py
+++ pythoncode/main.py
@@ -18,4 +18,4 @@ def index():
 
 
 if __name__ == '__main__':
-    socketio.run(app,host="192.168.1.250",allow_unsafe_werkzeug=True)
+    socketio.run(app,host="10.40.1.213",allow_unsafe_werkzeug=True)
